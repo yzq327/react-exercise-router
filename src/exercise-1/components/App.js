5c3d03fd9a18379abbffd334a0999c7843ee9440
yzq <you@example.com>
2020-09-23 22:44:45 +0800

    [yzq] router-is -finished

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -10,33 +10,31 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="app">
-            <nav className='header'>
-                <ul className='nav nav1'>
-                    <li className='aboutus'>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li className='products'>
-                        <Link to='/products'>Products</Link>
-                    </li>
-                    <li>
-                      <Link to='/myprofile'>My profile</Link>
-                    </li>
-                    <li >
+          <div className="app">
+              <div className='header'>             
+                    <div className='hdeaderInfo'>
                       <Link to='/aboutUs'>About us</Link>
-                    </li>                        
-                </ul>
-            </nav>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/products' component={Products} />
-            <Route exact path='/products/:id' component={Details} />
-            <Route exact path='/myprofile' component={MyProfile} />
-            <Route path='/aboutUs' component={AboutUs} />            
-            <Route path='/goods' component={Products} />
-            <Route component={Home} />
+                    </div>  
+                    <div className='hdeaderInfo'>
+                      <Link to='/myprofile'>My profile</Link>
+                    </div>
+                    <div className='hdeaderInfo'>
+                        <Link to='/products'>Products</Link>
+                    </div>                     
+                    <div className='hdeaderInfo'>
+                        <Link to='/'>Home</Link>
+                    </div>
+              </div>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/products' component={Products} />
+              <Route exact path='/products/:id' component={Details} />
+             <Route exact path='/myprofile' component={MyProfile} />
+             <Route path='/aboutUs' component={AboutUs} />            
+             <Route path='/goods' component={Products} />
+             <Route component={Home} />
           </Switch>
-        </div>
+          </div>
       </BrowserRouter>
     );
   }
